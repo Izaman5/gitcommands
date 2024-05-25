7534e7fe21b12d0ee48f0e4670bd35f2a9c3f39e
Trell <144071009+Izaman5@users.noreply.github.com>
2024-05-25 19:32:17 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ terraform {
 
 provider google {
   # Configuration options
-  credentials = "astral-option-419300-27d9d5cffd84.json"
+  credentials = "******.json"
   region = "asia-east1"
   zone = "asia-east1-a"
-  project = "astral-option-419300"
+  project = "***********"
   }
   
